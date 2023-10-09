0590d1530c540fa7d8b4c85b4d8a26ffc43700e6
Kevin J. Monter Zea <kjms.15987@gmail.com>
2023-10-09 17:41:14 -0500

    layout

---
+++ app/(dashboard)/_components/sidebar.tsx
@@ -0,0 +1,9 @@
+const Sidebar = () => {
+    return ( 
+        <div className="h-full border-r flex flex-col overflow-y-auto bg-whit shadow-sm">
+            sidebar!
+        </div>
+     );
+}
+ 
+export default Sidebar;
