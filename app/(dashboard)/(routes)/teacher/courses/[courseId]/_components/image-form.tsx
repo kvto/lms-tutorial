e4b3ad19e7cf4d651c8edd4d3dc38d687fc8590b
Kevin J. Monter Zea <kjms.15987@gmail.com>
2023-10-31 17:34:42 -0500

    course price form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/image-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/image-form.tsx
@@ -2,27 +2,18 @@
 
 import * as z from "zod";
 import axios from "axios";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { Pencil, PlusCircle } from "lucide-react";
+import { Pencil, PlusCircle, ImageIcon } from "lucide-react";
 import { useState } from "react";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
 import { Course } from "@prisma/client";
+import Image from "next/image";
 
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormMessage,
-} from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
-import { Textarea } from "@/components/ui/textarea";
+import { FileUpload } from "@/components/file-upload";
 
 interface ImageFormProps {
-  initialData: Course;
+  initialData: Course
   courseId: string;
 };
 
@@ -42,15 +33,6 @@ export const ImageForm = ({
 
   const router = useRouter();
 
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      imageUrl: initialData?.description || ""
-    },
-  });
-
-  const { isSubmitting, isValid } = form.formState;
-
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
       await axios.patch(`/api/courses/${courseId}`, values);
@@ -67,14 +49,14 @@ export const ImageForm = ({
       <div className="font-medium flex items-center justify-between">
         Course image
         <Button onClick={toggleEdit} variant="ghost">
-          {isEditing &&  (
+          {isEditing && (
             <>Cancel</>
           )}
           {!isEditing && !initialData.imageUrl && (
-           <>
-           <PlusCircle className="h-4 w-4 mr-2" />
-           Add an image
-         </> 
+            <>
+              <PlusCircle className="h-4 w-4 mr-2" />
+              Add an image
+            </>
           )}
           {!isEditing && initialData.imageUrl && (
             <>
@@ -85,45 +67,35 @@ export const ImageForm = ({
         </Button>
       </div>
       {!isEditing && (
-        <p className={cn(
-          "text-sm mt-2",
-          !initialData.description && "text-slate-500 italic"
-        )}>
-          {initialData.description || "No description"}
-        </p>
+        !initialData.imageUrl ? (
+          <div className="flex items-center justify-center h-60 bg-slate-200 rounded-md">
+            <ImageIcon className="h-10 w-10 text-slate-500" />
+          </div>
+        ) : (
+          <div className="relative aspect-video mt-2">
+            <Image
+              alt="Upload"
+              fill
+              className="object-cover rounded-md"
+              src={initialData.imageUrl}
+            />
+          </div>
+        )
       )}
       {isEditing && (
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmit)}
-            className="space-y-4 mt-4"
-          >
-            <FormField
-              control={form.control}
-              name="description"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Textarea
-                      disabled={isSubmitting}
-                      placeholder="e.g. 'This course is about...'"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <div className="flex items-center gap-x-2">
-              <Button
-                disabled={!isValid || isSubmitting}
-                type="submit"
-              >
-                Save
-              </Button>
-            </div>
-          </form>
-        </Form>
+        <div>
+          <FileUpload
+            endpoint="courseImage"
+            onChange={(url) => {
+              if (url) {
+                onSubmit({ imageUrl: url });
+              }
+            }}
+          />
+          <div className="text-xs text-muted-foreground mt-4">
+            16:9 aspect ratio recommended
+          </div>
+        </div>
       )}
     </div>
   )
--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,27 +1,28 @@
 "use client";
 
-import { UploadDropzone } from "@/lib/uploadthing";
-import { ourFileRouter } from "@/app/api/uploadthing/core";
 import toast from "react-hot-toast";
 
+import { UploadDropzone } from "@/lib/uploadthing";
+import { ourFileRouter } from "@/app/api/uploadthing/core";
+
 interface FileUploadProps {
-    onChange: (url?: string) => void;
-    endpoint: keyof typeof ourFileRouter;
+  onChange: (url?: string) => void;
+  endpoint: keyof typeof ourFileRouter;
 };
 
 export const FileUpload = ({
-    onChange,
-    endpoint
+  onChange,
+  endpoint
 }: FileUploadProps) => {
-    return(
-        <UploadDropzone
-        endpoint={endpoint} 
-        onClientUploadComplete={(res) => {
-            onChange(res?.[0].url);
-        }}
-        
-        onUploadError={(error: Error) => {
-            toast.error(`${error?.message}`)
-        }}/>
-    )
+  return (
+    <UploadDropzone
+      endpoint={endpoint}
+      onClientUploadComplete={(res) => {
+        onChange(res?.[0].url);
+      }}
+      onUploadError={(error: Error) => {
+        toast.error(`${error?.message}`);
+      }}
+    />
+  )
 }
