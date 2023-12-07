b34f562f48d073b5de5322d7ebdaf0fb097d155a
Kevin J. Monter Zea <kjms.15987@gmail.com>
2023-12-07 09:13:47 -0500

    chapter edit page ui

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
@@ -20,6 +20,7 @@ import {
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { Input } from "@/components/ui/input";
+import { ChaptersList } from "./chapters-list";
 
 
 
@@ -142,7 +143,10 @@ export const ChaptersForm = ({
           !initialData.chapters.length && "text-slate-500 italic"
         )}>
           {!initialData.chapters.length && "No chapters"}
-          
+          <ChaptersList
+          onEdit={()=> {}}
+          onReorder={onReorder}
+          items={initialData.chapters || []}/>
         </div>
       )}
       {!isCreating && (
