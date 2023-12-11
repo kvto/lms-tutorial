0f3d4c94b6901d342801bbed63aa251ed2e8887a
Kevin J. Monter Zea <kjms.15987@gmail.com>
2023-12-11 17:20:18 -0500

    Course Publish Action

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -8,6 +8,8 @@ import { ChapterTitleForm } from "./_components/chapter-title-form";
 import { ChapterdescriptionForm } from "./_components/chapter-description-form";
 import { ChapterAccessForm } from "./_components/chapter-access-form";
 import { ChapterVideoForm } from "./_components/chapter-video-form";
+import { Banner } from "@/components/banner";
+import { ChapterActions } from "./_components/chapter-actions";
 
 const ChapterIdPage = async ({
     params
@@ -42,7 +44,15 @@ const ChapterIdPage = async ({
     const totalFields = requiredFields.length;
     const completedFields = requiredFields.filter(Boolean).length;
     const completionText = `(${completedFields}/${totalFields})`
+    
+    const isComplete = requiredFields.every(Boolean);
     return ( 
+        <>
+        {!chapter.isPublished && (
+            <Banner
+            variant="warning"
+            label="This chapter is unpublished. It will not be visible in the course" />
+        )}
         <div className="p-6">
             <div className="flex items-center justify-between">
                  <div className="w-full">
@@ -61,6 +71,11 @@ const ChapterIdPage = async ({
                                 Complete all fields {completionText}
                             </span>
                         </div>
+                        <ChapterActions
+                        disabled={!isComplete}
+                        courseId={params.courseId}
+                        chapterId={params.chapterId}
+                        isPublished={chapter.isPublished}/>
                     </div>
                  </div>
             </div>
@@ -107,6 +122,7 @@ const ChapterIdPage = async ({
              </div>
             </div>
         </div>    
+        </>
      );
 }
  
