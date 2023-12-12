72fa17642f11e41d8f8e21f28e13cd6ed8aa2e38
Kevin J. Monter Zea <kjms.15987@gmail.com>
2023-12-12 17:07:53 -0500

    Course Grid and Card UI

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,5 +1,6 @@
 import { db } from "@/lib/db";
 import { Categories } from "./_components/categories";
+import { SearchInput } from "@/components/search-input";
 
 const SearchPag = async () => {
     const categories = await db.category.findMany({
@@ -8,10 +9,15 @@ const SearchPag = async () => {
         }
     })
     return ( 
+        <>
+        <div className="px-6 pt-6 md:hidden md:mb-0 block">
+            <SearchInput />
+        </div>
         <div className="p-6">
             <Categories
             items={categories}/>
         </div>
+        </>
      );
 }
  
