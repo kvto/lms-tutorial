b5a205bb3b988b617ee8d0295460af1168893715
Kevin J. Monter Zea <kjms.15987@gmail.com>
2023-12-13 10:15:52 -0500

    course and chapters layout

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,21 +1,46 @@
 import { db } from "@/lib/db";
 import { Categories } from "./_components/categories";
 import { SearchInput } from "@/components/search-input";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { getCourses } from "@/actions/get-courses";
+import { CoursesList } from "@/components/courses-list";
 
-const SearchPag = async () => {
+interface SearchPageProps {
+    searchParams: {
+        title: string;
+        categoryId: string;
+    }
+}
+
+const SearchPag = async ({
+    searchParams
+}: SearchPageProps) => {
+    const {userId} = auth();
+
+    if(!userId){
+        return redirect("/")
+    }
+
     const categories = await db.category.findMany({
         orderBy: {
             name: "asc"
         }
+    })
+
+    const courses = await getCourses({
+        userId,
+        ...searchParams,
     })
     return ( 
         <>
         <div className="px-6 pt-6 md:hidden md:mb-0 block">
             <SearchInput />
         </div>
-        <div className="p-6">
+        <div className="p-6 space-y-4">
             <Categories
             items={categories}/>
+            <CoursesList items={courses}/>
         </div>
         </>
      );
